bd2683d592c35539458957d921fda24c7e29359e
Igor Rodrigues <igor220597@hotmail.com>
2022-11-05 19:07:15 -0300

    feat: adiciona camada de repositorio

---
+++ repository/repository.go
@@ -0,0 +1,41 @@
+package gamesrepo
+
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/igudgz/campo-minado/entity"
+)
+
+type repo struct {
+	kvs map[string][]byte
+}
+
+func NewRepo() *repo {
+	return &repo{kvs: map[string][]byte{}}
+}
+
+func (repo *repo) Get(id string) (entity.Game, error) {
+	if value, ok := repo.kvs[id]; ok {
+		game := entity.Game{}
+		err := json.Unmarshal(value, &game)
+		if err != nil {
+			return entity.Game{}, errors.New("fail to get value from kvs")
+		}
+
+		return game, nil
+	}
+
+	return entity.Game{}, errors.New("game not found in kvs")
+}
+
+func (repo *repo) Save(game entity.Game) error {
+	bytes, err := json.Marshal(game)
+	if err != nil {
+		return errors.New("game fails at marshal into json string")
+	}
+
+	repo.kvs[game.ID] = bytes
+
+	return nil
+}
