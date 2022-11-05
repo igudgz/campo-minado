e2892b2f28d5029fe4d7851e720641dc80a0b4ba
Igor Rodrigues <igor220597@hotmail.com>
2022-11-05 18:49:57 -0300

    feat: separa entidades e adiciona funcoes do board

--- entity/game.go
+++ entity/game.go
@@ -1,5 +1,11 @@
 package entity
 
+const (
+	GAME_STATE_WON  = "won"
+	GAME_STATE_LOST = "lost"
+	GAME_STATE_NEW  = "new"
+)
+
 type Game struct {
 	ID            string        `json:"id"`
 	Name          string        `json:"name"`
@@ -8,12 +14,21 @@ type Game struct {
 	Board         Board         `json:"board"`
 }
 
-type BoardSettings struct {
-	Size  uint `json:"size"`
-	Bombs uint `json:"bombs"`
+func NewGame(id string, name string, size uint, bombs uint) Game {
+	return Game{
+		ID:    id,
+		Name:  name,
+		State: GAME_STATE_NEW,
+		BoardSettings: BoardSettings{
+			Size:  size,
+			Bombs: bombs,
+		},
+		Board: NewBoard(size, bombs),
+	}
 }
-
-type Board [][]string
+func (game *Game) IsOver() bool {
+	return game.State == GAME_STATE_LOST || game.State == GAME_STATE_WON
+}
 
 type GamesRepository interface {
 	Get(id string) (Game, error)
